65d06926331906b8001465e346bad5329fe53fb6
BLINKSK <48210490+BLINKSK@users.noreply.github.com>
2020-08-05 14:38:44 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$("document").ready(function () {
+    var qrCode = new QRCode("qrCode", {
+        text: "               " + app.qrCode + "               ",
+        width: 150,
+        height: 150,
+        colorDark: app.colorOfCode,
+        correctLevel: QRCode.CorrectLevel.H
+    });
+    $("#menuToggler").click(function () {
+        $(".floatMenu").toggle("fast");
+        qrCode.clear();
+        document.getElementById("qrCode").innerHTML = "";
+        if (app.qrCode === "") {
+            app.qrCode = app.generateCode();
+        }
+        qrCode = new QRCode("qrCode", {
+            text: "               " + app.qrCode + "               ",
+            width: 150,
+            height: 150,
+            colorDark: app.colorOfCode,
+            correctLevel: QRCode.CorrectLevel.H
+        });
+    });
+    $(".submitButton").click(function () {
+        $(".floatMenu").toggle("fast");
+        qrCode.clear();
+        document.getElementById("qrCode").innerHTML = "";
+        if (app.qrCode === "") {
+            app.qrCode = app.generateCode();
+        }
+        qrCode = new QRCode("qrCode", {
+            text: "               " + app.qrCode + "               ",
+            width: 150,
+            height: 150,
+            colorDark: app.colorOfCode,
+            correctLevel: QRCode.CorrectLevel.H
+        });
+    });
+});
